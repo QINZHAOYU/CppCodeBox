0b2a32e8c18eeb468959739ec5fb25867fe6a327
qinzhaoyu <17764586230@163.com>
2021-10-22 17:12:10 +0800

    start coding

---
+++ modules/RiverNetAutoInit.cpp
@@ -0,0 +1,6 @@
+# include "RiverNetAutoInit.hpp"
+
+
+
+
+
---
+++ modules/RiverNetAutoInit.hpp
@@ -0,0 +1,18 @@
+/** *****************************************************************************
+*   @copyright :  Copyright (C) 2021 Qin ZhaoYu. All rights reserved.
+*
+*   @author    :  Qin ZhaoYu.
+*   @see       :  https://github.com/QINZHAOYU
+*   @brief     :  To provide an automatically initialization method of river net.
+*
+*   Change History:
+*   -----------------------------------------------------------------------------
+*   --version-1.0, 2021/10/22, Qin ZhaoYu,
+*   Init model.
+*
+** ******************************************************************************/
+
+#pragma once
+#include "common/CommHeader.hpp"
+
+
