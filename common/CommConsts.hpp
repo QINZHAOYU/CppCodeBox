20bb84f39c0f43c73526d871564169076f98f6c6
qinzhaoyu <17764586230@163.com>
2021-10-27 10:59:15 +0800

    add built-in types limits

--- common/CommConsts.hpp
+++ common/CommConsts.hpp
@@ -1,4 +1,12 @@
 #pragma once
 #include "CommHeader.hpp"
+#include <limits>
 
+
+const int _INT_MAX           = (std::numeric_limits<int>::max)();
+const unsigned int _UINT_MAX = (std::numeric_limits<unsigned int>::max)();
+const double _DBL_MAX        = (std::numeric_limits<double>::max)();
+const double _DBL_EPSILON    = (std::numeric_limits<double>::epsilon)();
+const float _FLOAT_MAX       = (std::numeric_limits<float>::max)();
+const float _FLOAT_EPSILON   = (std::numeric_limits<float>::epsilon)();
 
