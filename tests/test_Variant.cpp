8a13a58b1f991bb14d8ecccf19866f8a2a7ee734
qinzhaoyu <17764586230@163.com>
2022-01-12 20:17:05 +0800

    test(Variant): tests worked

--- tests/test_Variant.cpp
+++ tests/test_Variant.cpp
@@ -7,16 +7,14 @@ using namespace ccb;
 TEST_CASE("tests for class Variant")
 {
     SECTION("test1")
-    {
-        // failed.
-
-        // using CV = Variant<int, double, string, int>;
-        // CV var = 10;
+    { 
+        using CV = Variant<int, double, string, int>;
+        CV var = 10;
     
-        // CHECK(std::is_same<CV::IndexType<1>, double>::value == true);
-        // cout << typeid(CV::IndexType<1>).name() << endl;
+        CHECK(std::is_same<CV::IndexType<1>, double>::value == true);
+        cout << typeid(CV::IndexType<1>).name() << endl;
 
-        // CHECK(var.indexOf<string>() == 2);
+        CHECK(var.GetIndexOf<string>() == 2);
     }
 }
 
