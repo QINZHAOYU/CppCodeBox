dd6ccb09db925852623e6c1fdc47592b96cf0924
qinzhaoyu <17764586230@163.com>
2022-01-13 10:37:05 +0800

    feat(TulpeHelper): add function apply

--- modules/TupleHelper.hpp
+++ modules/TupleHelper.hpp
@@ -4,6 +4,22 @@
 *   @author    :  Qin ZhaoYu.
 *   @brief     :  To implement some external tuple helper class.
 *
+*   because the `k` is not appeared in input arguments, therefore,
+*   there is no automatic derivation of its type. While `Tuple` does.
+*  
+*   template can be instantiated (partial) explicitly and implicitly.
+*   explicit instantiation can improve efficiency. it used as
+*   `tempalte void func<T>(T)`; It is also important to distinguish
+*   between explicit instantiation and explicit materialization.
+*  
+*   template can be specialized by templates.
+*   template can be overloaded.
+*  
+*   template parameters can be non-type(built-on types, limited to
+*   + int/char/long/unsigned/bool/short/size_t, 
+*   + pointers and lvalue references to objects)
+*   which must be compile-time constants, a constant of a fixed type.
+*
 *   Change History:
 *   -----------------------------------------------------------------------------
 *   --version-1.0, 2022/01/12, Qin ZhaoYu,
@@ -46,7 +62,7 @@ void PrintTuple(const tuple<Args...> &t)
 }
 
 
-/// \brief Namespace to contains index searching details.
+/// \brief Namespace to contains tuple function details.
 namespace detail
 {
 /// \brief To directly compare values of convertible types.
@@ -104,22 +120,6 @@ GetArgByIndex(size_t index, Tuple &tp)
     throw std::invalid_argument("arg index out of range.");
 }
 
-// because the `k` is not appeared in input arguments, therefore,
-// there is no automatic derivation of its type. While `Tuple` does.
-//
-// template can be instantiated (partial) explicitly and implicitly.
-// explicit instantiation can improve efficiency. it used as
-// `tempalte void func<T>(T)`; It is also important to distinguish
-// between explicit instantiation and explicit materialization.
-//
-// template can be specialized by templates.
-// template can be overloaded.
-//
-// template parameters can be non-type(built-on types, limited to
-// + int/char/long/unsigned/bool/short/size_t, 
-// + pointers and lvalue references to objects)
-// which must be compile-time constants, a constant of a fixed type.
-
 template<size_t k=0, typename Tuple>
 typename std::enable_if<(k < std::tuple_size<Tuple>::value)>::type
 GetArgByIndex(size_t index, Tuple &tp)
@@ -135,6 +135,28 @@ GetArgByIndex(size_t index, Tuple &tp)
 }
 
 
+/// \brief To apply tuple to funciton as its parameters.
+template<int...>
+struct IndexTuple
+{};
+
+template<int N, int... Indexes>
+struct MakeIndexes : MakeIndexes<N-1, N-1, Indexes...>
+{};
+
+template<int... indexes>
+struct MakeIndexes<0, indexes...>
+{
+    using type = typename IndexTuple<indexes...>;
+};
+
+template<typename F, typename Tuple, int... Indexes>
+auto apply(F&& f, IndexTuple<Indexes...>&& in, Tuple&& tp) 
+-> decltype(std::forward<F>(f)(std::get<Indexes>(tp)...))
+{
+    // tuple tp expanded as arguments of function f.
+    return std::forward<F>(f)(std::get<Indexes>(tp)...); 
+}
 
 
 }
